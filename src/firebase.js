70944a0e189375077f9c105419cd266df9701ef5
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-11-26 14:29:21 +0100

    c-r-a and firebase installed

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+var config = {
+    apiKey: "AIzaSyC3ijE0n-Ogy3uTATxFcYrxmye536PBNcM",
+    authDomain: "krystian-kaminski.firebaseapp.com",
+    databaseURL: "https://krystian-kaminski.firebaseio.com",
+    projectId: "krystian-kaminski",
+    storageBucket: "krystian-kaminski.appspot.com",
+    messagingSenderId: "185496303101"
+  };
+  
+const firebaseApp = firebase.initializeApp(config);
+export const database = firebaseApp.database();
